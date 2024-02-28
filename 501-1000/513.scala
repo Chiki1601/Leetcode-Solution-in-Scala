f583f6ab2718d89f7e4cde05353619889b1e6dff
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2024-02-28 10:42:53 +0530

    Update 513.scala

--- 501-1000/513.scala
+++ 501-1000/513.scala
@@ -8,25 +8,17 @@
  */
 object Solution {
     def findBottomLeftValue(root: TreeNode): Int = {
-            def findDepth(node: TreeNode):Int = {
-       node match{
-         case null => 0
-         case _ => math.max(findDepth(node.left), findDepth(node.right)) + 1
-       }
-     }
+        def traversal(level: List[TreeNode], leftValue: Int): Int = {
+            level match {
+                case Nil    => leftValue
+                case _      =>
+                    val nextlevel = level.flatMap(nd => List(Option(nd.left), Option(nd.right)).flatten)
+                    val newLeftValue = level.head.value
+                    traversal(nextlevel, newLeftValue)
+            }
+        }
 
-  def helper(node: TreeNode, remainDepth: Int) :Option[Int] = {
-    (node, remainDepth) match {
-      case (null, _) => None
-      case (_, 1) => Some(node.value)
-      case (n,d) => {
-        var left = helper(n.left, d-1)
-        if (!left.isDefined) helper(n.right, d-1)
-        else Some(left.get)
-      } 
-    }
-  }
-  val depth = findDepth(root)
-  helper(root, depth).get
+        traversal(List(root), root.value)
+            
     }
 }
